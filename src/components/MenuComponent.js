e62c23fdeb510ac354f424b6fa50b0a84b411a01
Shubham <princeshubham22699@gmail.com>
2018-08-26 20:54:57 +0530

    Assignment-1 React

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -18,19 +18,60 @@ class Menu extends Component {
     renderDish(dish) {
         if(dish !=null){
             return(
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle> {dish.name} </CardTitle>
-                        <CardText> {dish.description} </CardText>                        
-                    </CardBody>    
-                </Card>    
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle> {dish.name} </CardTitle>
+                            <CardText> {dish.description} </CardText>                        
+                        </CardBody>    
+                    </Card>
+                </div>    
             );
         }
         else{
             return(
                 <div></div>
             );
+        }
+    }
+
+
+    renderComment(dish) {
+
+        if(dish!=null){
+            const month =["Jan" , "Feb" , "Mar" 
+                        , "Apr" , "May" , "Jun" 
+                        , "Jul" , "Aug" , "Sep" 
+                        , "Oct" , "Nov" , "Dec"]
+        
+            const comment = dish.comments.map((text) => {
+                const date = text.date.split('T')[0].split("-").reverse().join("-").split("-");
+                return(
+                    <div>
+                        <p>
+                            {text.comment}
+                        </p>
+                        <p>
+                           -- {text.author} , {month[parseInt(date[1])-1]} {date[0]}, {date[2]}
+                        </p>
+                    </div>
+                );
+            });
+
+            return(
+                <div className="col-12 col-md-5 m-1">
+                    <h4>
+                        Comment
+                    </h4>
+                    {comment}
+                </div>
+            )
+        }
+        else{
+            return(
+                <div></div>
+            )
         }
     }
 
@@ -58,6 +99,7 @@ class Menu extends Component {
                 </div>
                 <div className="row">
                     {this.renderDish(this.state.selectedDish)}
+                    {this.renderComment(this.props.dishes[0])}
                 </div>
             </div>
         );
