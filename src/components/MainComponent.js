811a083570f96ef34f8c8e24b6309fe8eae9c899
Shubham <princeshubham22699@gmail.com>
2018-11-08 01:13:51 +0530

    All routes added

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent'
@@ -41,13 +42,14 @@ class Main extends Component {
       );
     };
 
-    const DishWithId = ({match}) => {
+    const DishWithId = ({ match }) => {
       return (
-        <DishDetail dish = {this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} 
-          comments = {this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
-          />
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+        />
       );
     };
+
 
     return (
       <div>
@@ -57,6 +59,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
